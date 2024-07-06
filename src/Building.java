f19410d77bd0f607df58e1312f989d59826833d8
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-07-06 12:04:56 -0500

    Create Business Subclass of Building Superclass

--- src/Building.java
+++ src/Building.java
@@ -1,4 +1,95 @@
 
 public class Building {
 
+	// Instance fields
+	protected String projectName;
+	protected String completeAddress;
+	protected double totalSquareFeet;
+	protected String occupancyGroup;
+	protected String subgroup;
+	
+	
+	//Constructors
+	public Building() {
+		this.projectName = "";
+		this.completeAddress = "";
+		this.totalSquareFeet = 0.0;
+		this.occupancyGroup = "";
+		this.subgroup = "";
+	} // End of empty-argument constructor
+	
+	public Building(String projectName, String completeAddress, double totalSquareFeet, String occupancyGroup, String subgroup) {
+		this.projectName = projectName;
+		this.completeAddress = completeAddress;
+		this.totalSquareFeet = totalSquareFeet;
+		this.occupancyGroup = occupancyGroup;
+		this.subgroup = subgroup;
+	} // End of preferred constructor
+	
+	
+	// Getters and Setters
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getCompleteAddress() {
+		return completeAddress;
+	}
+
+	public void setCompleteAddress(String completeAddress) {
+		this.completeAddress = completeAddress;
+	}
+
+	public double getTotalSquareFeet() {
+		return totalSquareFeet;
+	}
+
+	public void setTotalSquareFeet(double totalSquareFeet) {
+		this.totalSquareFeet = totalSquareFeet;
+	}
+
+	public String getOccupancyGroup() {
+		return occupancyGroup;
+	}
+
+	public void setOccupancyGroup(String occupancyGroup) {
+		this.occupancyGroup = occupancyGroup;
+	}
+
+	public String getSubgroup() {
+		return subgroup;
+	}
+
+	public void setSubgroup(String subgroup) {
+		this.subgroup = subgroup;
+	}
+	// End of Getters and Setters
+	
+	
+	public void Draw() {
+		System.out.println("Drawing code for " + this.getClass());
+	} // End of Draw
+
+	public String displayData() {
+		StringBuilder data = new StringBuilder("Project Name: " + getProjectName() + "\n");
+		data.append("Address: " + getCompleteAddress() + "\n");
+		data.append("Square Feet: " + getTotalSquareFeet() + "\n");
+		data.append("Occupancy Group: " + getOccupancyGroup() + "\n");
+		data.append("Occupancy Subgroup: " + getSubgroup() + "\n");
+		
+		return data.toString();
+	} // End of displayData
+
+	@Override
+	public String toString() {
+		return "Building [projectName=" + projectName + ", completeAddress=" + completeAddress + ", totalSquareFeet="
+				+ totalSquareFeet + ", occupancyGroup=" + occupancyGroup + ", subgroup=" + subgroup + "]";
+	}
+	
+	
+	
 }
---
+++ src/Business.java
@@ -0,0 +1,51 @@
+
+public class Business extends Building {
+
+	// Instance fields
+	protected int numRentableUnits;
+	
+	// Constructors
+	public Business() {
+		super();
+		this.numRentableUnits = 0;
+		
+	} // End of empty-argument constructor
+	
+	
+	public Business(String projectName, String completeAddress, double totalSquareFeet, String occupancyGroup, String subgroup, int numRentableUnits) {
+		super(projectName, completeAddress, totalSquareFeet, occupancyGroup, subgroup);
+		this.numRentableUnits = numRentableUnits;
+	} // End of preferred constructor
+	
+
+	// Getters and Setters
+	public int getNumRentableUnits() {
+		return numRentableUnits;
+	}
+
+
+	public void setNumRentableUnits(int numRentableUnits) {
+		this.numRentableUnits = numRentableUnits;
+	}
+	// End of Getters and Setters
+
+	
+	public String displayData() {
+		StringBuilder data = new StringBuilder(super.displayData());
+		data.append("Rentable Units: " + getNumRentableUnits() + "\n");
+		
+		return data.toString();
+	} // End of displayData
+
+	@Override
+	public String toString() {
+		return "Business [numRentableUnits=" + numRentableUnits + ", projectName=" + projectName + ", completeAddress="
+				+ completeAddress + ", totalSquareFeet=" + totalSquareFeet + ", occupancyGroup=" + occupancyGroup
+				+ ", subgroup=" + subgroup + "]";
+	}
+	
+	
+	
+	
+	
+}
