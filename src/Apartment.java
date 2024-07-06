495645dc8465d0d972d1f1a87d7702f6e958c4eb
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-07-06 13:00:52 -0500

    Create Apartment Subclass of Residential Superclass

---
+++ src/Apartment.java
@@ -0,0 +1,72 @@
+
+public class Apartment extends Residential {
+
+	// Instance Fields
+	private int numRentableUnits;
+	private double avgUnitSize;
+	private boolean parkingAvailable;
+	
+	// Constructors
+	public Apartment() {
+		super();
+		this.numRentableUnits = 0;
+		this.avgUnitSize = 0.0;
+		this.parkingAvailable = false;
+	} // End of empty-argument constructor
+	
+	public Apartment(String projectName, String completeAddress, double totalSquareFeet, String occupancyGroup, String subgroup, int numBedrooms, int numBathrooms, boolean laundryRoom, int numRentableUnits, double avgUnitSize, boolean parkingAvailable) {
+		
+		super(projectName, completeAddress, totalSquareFeet, occupancyGroup, subgroup, numBedrooms, numBathrooms, laundryRoom);
+		this.numRentableUnits = numRentableUnits;
+		this.avgUnitSize = avgUnitSize;
+		this.parkingAvailable = parkingAvailable;
+		
+	} // End of preferred constructor
+
+	
+	// Getters and Setters
+	public int getNumRentableUnits() {
+		return numRentableUnits;
+	}
+
+	public void setNumRentableUnits(int numRentableUnits) {
+		this.numRentableUnits = numRentableUnits;
+	}
+
+	public double getAvgUnitSize() {
+		return avgUnitSize;
+	}
+
+	public void setAvgUnitSize(double avgUnitSize) {
+		this.avgUnitSize = avgUnitSize;
+	}
+
+	public boolean isParkingAvailable() {
+		return parkingAvailable;
+	}
+
+	public void setParkingAvailable(boolean parkingAvailable) {
+		this.parkingAvailable = parkingAvailable;
+	}
+	// End of Getters and Setters
+	
+	public String displayData() {
+		StringBuilder data = new StringBuilder(super.displayData());
+		data.append("Rentable Units: " + getNumRentableUnits() + "\n");
+		data.append("Average Unit Size: " + getAvgUnitSize() + "\n");
+		data.append("Parking: " + isParkingAvailable() + "\n");
+		
+		return data.toString();
+	} // End of displayData
+
+	@Override
+	public String toString() {
+		return "Apartment [numRentableUnits=" + numRentableUnits + ", avgUnitSize=" + avgUnitSize
+				+ ", parkingAvailable=" + parkingAvailable + ", numBedrooms=" + numBedrooms + ", numBathrooms="
+				+ numBathrooms + ", laundryRoom=" + laundryRoom + ", projectName=" + projectName + ", completeAddress="
+				+ completeAddress + ", totalSquareFeet=" + totalSquareFeet + ", occupancyGroup=" + occupancyGroup
+				+ ", subgroup=" + subgroup + "]";
+	}
+
+	
+}
